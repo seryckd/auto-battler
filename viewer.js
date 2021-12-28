88273a56172c1d16a3b85c96037d6f0f486b6fbd
seryckd <7096418+seryckd@users.noreply.github.com>
2021-12-28 15:21:56 -0500

    Added attack, damage and death animations

--- viewer.js
+++ viewer.js
@@ -8,8 +8,9 @@ export class Viewer {
         this.setup(this.tray1.minions, 'tray1');
         this.setup(this.tray2.minions, 'tray2');
 
-        this.turnCount = 0;
+        this.turnCount = -1;
         this.actionCount = 0;
+        this.actionContext = {};
     }
 
     setup(minions, trayId) {
@@ -19,68 +20,79 @@ export class Viewer {
         });
     }
 
-    next() {
+    turn() {
+
+        this.turnCount++;
 
         if (this.turnCount >= this.combat.length) {
             return false;
         }
 
-        let turn = this.combat[this.turnCount];
+        console.log('turn:', this.turnCount);
 
-        let action = turn.changes[this.actionCount];
+        this.actionCount = -1;
+        this.actionContext = {};
+
+        this.action();
+    }
+
+    action() {
+        const me=this;
+        let actions = this.combat[this.turnCount].changes;
+        this.actionCount++;
+
+        if (this.actionCount >= actions.length) {
+            this.retreatAnimation(
+                this.actionContext.minion, 
+                this.actionContext.delta.dX, 
+                this.actionContext.delta.dY)
+                .onfinish = function() {
+                    console.log('end of turn');
+                }
+            return;
+        }
+        
+        let action = actions[this.actionCount];
+        let minion = document.getElementById(this.makeDomId(action.id));
 
         console.log(action);
 
-        if (action.action === 'attack') {
-            let attackMin = document.getElementById(this.makeDomId(action.id));
-            let targetMin = document.getElementById(this.makeDomId(action.targetId));
+        switch(action.action) {
+            case 'attack': {
+                this.actionContext.minion = minion;
 
-            const source = this.minionInfo(attackMin);
-            const target = this.minionInfo(targetMin);
+                this.actionContext.delta = this.deltaToTarget(
+                    minion,
+                    document.getElementById(this.makeDomId(action.targetId)));
 
-            // The source minion center moves towards the target minion center
-            // but stops before it gets there
-            const bigX = target.x - source.x;
-            const bigY = target.y - source.y;
-            const bigH = Math.sqrt(bigX*bigX + bigY*bigY);
-            const smallH = bigH - 1.5*target.radius;
-            const smallX = smallH * (bigX/bigH);
-            const smallY = smallH * (bigY/bigH);
+                this.attackAnimation(
+                    minion, 
+                    this.actionContext.delta.dX, 
+                    this.actionContext.delta.dY)
+                    .onfinish = function() {
+                        me.action();
+                    };
+                    
+                break;
+            }
 
-            const anim = attackMin.animate([
-                { transform: 'translate(0px, 0px)'},
-                { transform: `translate(${smallX}px, ${smallY}px)` }
-            ], {
-                // timing options
-                duration: 1000,
-                iterations: 1
-            }).onfinish = function() {
-                console.log('finished 1st anim');
-                attackMin.animate([
-                    { transform: `translate(${smallX}px, ${smallY}px)` },
-                    { transform: 'translate(0px, 0px)'}    
-                ], {
-                    duration: 1000,
-                    iterations: 1    
-                });
-            };
+            case 'change': {
+                let attr = minion.getElementsByClassName(action.stat)[0];
+                this.damageAnimation(attr, action.value)
+                    .onfinish = function() {
+                        me.action();
+                    };
+                break;
+            }
 
-        } else if (action.action === 'change') {
-
-            let minion = document.getElementById(this.makeDomId(action.id));
-            let attr = minion.getElementsByClassName(action.stat)[0];
-            attr.innerText = action.value;
-
-        } else if (action.action === 'remove') {
-            document.getElementById(this.makeDomId(action.id)).remove();
+            case 'remove': 
+                this.deadAnimation(minion)
+                    .onfinish = function() {
+                        minion.remove();
+                        me.action();    
+                    }
+                break;
         }
-
-        if (++this.actionCount >= turn.changes.length) {
-            this.actionCount = 0;
-            ++this.turnCount;
-        }
-
-        return true;
     }
 
     makeDomId(id) {
@@ -104,15 +116,108 @@ export class Viewer {
         `;
 
         return min;
-     }
+    }
 
-     minionInfo(min) {
-         const r = min.getBoundingClientRect();
-         return {
-            x: r.left + r.width/2,
-            y: r.top + r.height/2,
-            radius: Math.max(r.width, r.height)/2
-         };
-     }
+    deltaToTarget(sourceMin, targetMin) {
+
+        const info = function(min) {
+            const r = min.getBoundingClientRect();
+            return {
+                x: r.left + r.width/2,
+                y: r.top + r.height/2,
+                radius: Math.max(r.width, r.height)/2
+            };    
+        }
+
+        // The source minion center moves towards the target minion center
+        // but stops before it gets there
+
+        const source = info(sourceMin);
+        const target = info(targetMin);
+
+        const bigX = target.x - source.x;
+        const bigY = target.y - source.y;
+        const bigH = Math.sqrt(bigX*bigX + bigY*bigY);
+        const smallH = bigH - 1.5*target.radius;
+
+        return {
+            dX: smallH * (bigX/bigH),
+            dY: smallH * (bigY/bigH)
+        }
+    }
+
+    attackAnimation(minion, x, y) {
+        return minion.animate(
+            [
+                {
+                    position: 'relative',
+                    left: 0,
+                    top: 0
+                },
+                {
+                    position: 'relative',
+                    left: `${x}px`,
+                    top: `${y}px`
+                }
+                /*
+                { transform: 'translate(0px, 0px)'},
+                { transform: `translate(${x}px, ${y}px)` }
+                */
+            ], {
+                duration: 400,
+                fill: 'forwards',
+                iterations: 1
+            });
+    }
+
+    retreatAnimation(minion, x, y) {
+        return minion.animate(
+            [
+                {
+                    position: 'relative',
+                    left: `${x}px`,
+                    top: `${y}px`
+                },
+                {
+                    position: 'relative',
+                    left: 0,
+                    top: 0
+                }
+            ], {
+                duration: 500,
+                fill: 'forwards',
+                iterations: 1
+            })
+    }
+
+    damageAnimation(element, newValue) {
+
+        element.innerText = newValue;
+
+        return element.animate(
+            [
+                {
+                    color: 'blue'
+                },
+                {
+                    color: 'white'
+                }
+            ], {
+                duration: 500,
+                iterations: 1
+            }
+        );
+    }
+
+    deadAnimation(minion) {
+        return minion.animate([
+            { opacity: '100%' },
+            { opacity: '0%' }
+        ], {
+            duration: 500,
+            fill: 'forwards',
+            iterations: 1
+        });
+    }
 }
 
